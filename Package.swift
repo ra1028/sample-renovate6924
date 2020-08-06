a11e2e7eb7445db0d4df0e1982b78c1a5c4398ae
Ryo Aoyama <r.fe51028.r@gmail.com>
2020-08-06 15:53:17 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "merc",
+    platforms: [.macOS(.v10_16)],
+    products: [.executable(name: "sample", targets: ["sample"])],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.1.0")),
+    ],
+    targets: [
+        .target(
+            name: "sample",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,1 @@
+Sample repository for the issue [#6924](https://github.com/renovatebot/renovate/issues/6924) on [renovatebot/renovate](https://github.com/renovatebot/renovate)
